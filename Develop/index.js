0e35197bd6396c470c4db80b53b07221669c990c
Knocktov <mitch.vandermolen@gmail.com>
2022-06-05 22:41:23 -0400

    first code iteration

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,100 @@
-// TODO: Include packages needed for this application
 
-// TODO: Create an array of questions for user input
-const questions = [];
+const inquirer = require("inquirer");
+const generateMarkdown = require("./utils/generateMarkdown");
+const fs = require("fs");
+const util = require("util");
+const writeFileAsync = util.promisify(fs.writeFile);
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function promptUser() {
+	return inquirer.prompt([
+		{
+			type: "input",
+			message: "What is the name of your project?",
+			name: "title",
+		},
+		{
+			type: "input",
+			message: "Please write a 2-3 sentence description of the project.",
+			name: "description",
+		},
+		{
+			type: "input",
+			message: "What is the URL of the project's github repository?",
+			name: "repository",
+		},
+		{
+			type: "input",
+			message:
+				"What installation instructions should be included in the README?",
+			default: "npm i",
+			name: "installation",
+		},
+		{
+			type: "input",
+			message: "What usage information would you like to include?",
+			name: "usage",
+		},
+		{
+			type: "list",
+			message: "What license does this project have?",
+			choices: ["MIT", "IPL 1.0", "MPL 2.0", "ODbL", "PDDL", "Perl"],
+			default: 0,
+			name: "license",
+		},
+		{
+			type: "input",
+			message: "Who should be listed as a contributor on this project?",
+			name: "contributors",
+		},
+		{
+			type: "input",
+			message:
+				"What instructions should be included regarding future contributions to this project?",
+			default:
+				"\n1. Fork the Project\n2. Create your Feature Branch (git checkout -b feature/AmazingFeature)\n3. Commit your Changes (git commit -m 'Add some AmazingFeature')\n4. Push to the Branch (git push origin feature/AmazingFeature)\n5. Open a Pull Request",
+			name: "contributions",
+		},
+		{
+			type: "input",
+			message: "Please provide an example of how to test run the code.",
+			name: "tests",
+		},
+		{
+			type: "input",
+			message:
+				"What additional questions still exist that you are trying to solve?",
+			name: "questions",
+		},
+		{
+			type: "input",
+			message: "What is your first and last name?",
+			name: "author",
+		},
+		{
+			type: "input",
+			message: "What is your email address?",
+			name: "email",
+		},
+		{
+			type: "input",
+			message: "What is the URL of your LinkedIn profile?",
+			name: "linkedin",
+		},
+	]);
+}
 
-// TODO: Create a function to initialize app
-function init() {}
+async function init() {
+	try {
+		
+		const data = await promptUser();
+		
+		const markup = generateMarkdown(data);
+		
+		await writeFileAsync("README.md", markup);
+	} catch (err) {
+		
+		console.log(err);
+	}
+}
 
-// Function call to initialize app
 init();
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,20 +1,128 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
-
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
-
-// TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
 
-`;
+	let licenseBadge = "";
+
+	if (data.license === "MIT") {
+		licenseBadge =
+			"MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)";
+	} else if (data.license === "IPL 1.0") {
+		licenseBadge =
+			"IPL 1.0](https://img.shields.io/badge/License-IPL%201.0-blue.svg)](https://opensource.org/licenses/IPL-1.0)";
+	} else if (data.license === "MPL 2.0") {
+		licenseBadge =
+			"MPL 2.0](https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)";
+	} else if (data.license === "ODbL") {
+		licenseBadge =
+			"ODbL](https://img.shields.io/badge/License-ODbL-brightgreen.svg)](https://opendatacommons.org/licenses/odbl/)";
+	} else if (data.license === "PDDL") {
+		licenseBadge =
+			"ODbL](https://img.shields.io/badge/License-PDDL-brightgreen.svg)](https://opendatacommons.org/licenses/pddl/)";
+	} else if (data.license === "Perl") {
+		licenseBadge =
+			"Artistic-2.0](https://img.shields.io/badge/License-Perl-0298c3.svg)](https://opensource.org/licenses/Artistic-2.0)";
+	}
+
+	return (
+		`
+  # **${data.title}**
+
+  ---
+  
+  ## **Description**
+  
+  ${data.description}
+  
+  ---
+  
+  ## **Table of Contents**
+  
+  1. [Installation](#Installation)
+  2. [Usage](#Usage)
+  3. [License](#License)
+  4. [Contributions](#Contributions)
+  5. [Tests](#Tests)
+  6. [Questions](#Questions)
+  7. [Author](#Author)
+  
+  ---
+  
+  ## **Installation**
+  
+  1. Clone the repo: ` +
+		"```git clone " +
+		data.repository +
+		".git```" +
+		`\n` +
+		`2. Install NPM packages: ` +
+		"```" +
+		data.installation +
+		"```" +
+		`
+  
+  ---
+  
+  ## **Usage**
+  
+  ${data.usage}
+  
+  ---
+  
+  ## **License**
+  
+  Distributed under the ${data.license} License.
+
+  [![License: ${licenseBadge}
+
+  ---
+  
+  ## **Contributions**
+  
+  **Current Contributors:**
+  
+  _${data.contributors}_
+  
+  Contributions are what make the open source community such an amazing place to be learn, inspire, and create. Any contributions you make are greatly appreciated.
+  
+  ${data.contributions}
+  
+  ---
+  
+  ## **Tests**
+
+  This application has been tested in a countless number of different environments to ensure peak performance. However, bugs may exist, especially when used in combination with additional packages. Please be sure to test the application after an initial download. 
+  
+  To test run the code after installing on your local device, use the following code:` +
+		`\n` +
+		"```" +
+		data.tests +
+		"```" +
+		`
+  
+  ---
+  
+  ## **Questions**
+
+  The following questions exist on our end, and are currently trying to be solved:
+
+  ${data.questions}
+  
+  ---
+  
+  ## **Author**
+
+  Below is contact information for the author of this application. Please feel free to reach out directly if additional questions exist.
+  
+  - Name: ${data.author}
+  - Email: ${data.email}
+  - LinkedIn: ${data.linkedin}
+
+  ---
+  
+  ### Thank you for using ${data.title}!
+
+  `
+	);
 }
 
 module.exports = generateMarkdown;
